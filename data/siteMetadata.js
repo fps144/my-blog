f5125253545134b1b3a743fcca309606c86d2ed1
Songyu <yusong1108@gmail.com>
2024-10-05 01:45:43 +0800

    Change logo

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   title: 'Sorain\'s Blog',
   author: 'Sorain',
   headerTitle: 'Sorain\'s Blog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'Happy Coding Happy Life.',
   language: 'zh-CN',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
